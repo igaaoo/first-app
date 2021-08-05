fae58f6d2faf12f313558809fc48c12c448ee533
Igor Neves <igorneves0404@gmail.com>
2021-08-05 19:57:39 -0300

    Arquivos Necessários

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port=3000;
+const address = 'localhost';
+//Criando um servidor simples com o Node.js e o Express
+const server = app.listen(port,address,()=>{
+ let host = server.address().address;
+ let port = server.address().port;
+ console.log(`Servidor executando no endereço ${host} e porta ${port}`);
+});
