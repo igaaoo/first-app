034e77123dee881871c7b22f655f87386cdc8e1e
Igor Neves <igorneves0404@gmail.com>
2021-08-11 13:21:01 -0300

    Atualização das rotas

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const app = express();
-const port=3000;
+const port = 3000;
 const routes = require("./routes");
 const address = 'localhost';
 const expressLayouts = require("express-ejs-layouts");
 
 
+global.users = [
+    { name: "Wellington W. F. Sarmento", address: "Rua Dom Jeronimo, 666", email: "wwagner@virtual.ufc.br", age: 46, height: 1.70, vote: true },
+    { name: "Patricia S. Paula", address: "Rua Dom Jeronimo, 666", email: "patricia@virtual.ufc.br", age: 32, height: 1.65, vote: true },
+    { name: "Henrique Sérgio L. Pequeno", address: "Rua do Henrique, 666", email: "henrique@virtual.ufc.br", age: 38, height: 1.82, vote: true }
+];
+
+
+
+
+
 app.set('view engine', 'ejs');
 app.use(expressLayouts)
 
+app.use(express.urlencoded({extended:false}));  //prepara a aplicacao para receber dados na forma de query string
+app.use(express.json());    //prepara a aplicacao para receber dados no formato JSON
+
 app.use('/', routes);
 //Criando um servidor simples com o Node.js e o Express
-const server = app.listen(port,address,()=>{
- let host = server.address().address;
- let port = server.address().port;
- console.log(`Servidor executando no endereço ${host} e porta ${port}`);
+const server = app.listen(port, address, () => {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log(`Servidor executando no endereço ${host} e porta ${port}`);
 });
--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 const app = express();
 
 const expressLayouts = require("express-ejs-layouts");
@@ -7,29 +7,37 @@ router.use(express.static('public'));
 
 
 
-router.get('/',(req,res)=>{
- res.render('pages/home.ejs');
- console.log("Acessou a home");
+router.get('/', (req, res) => {
+  res.render('pages/home.ejs');
+  console.log("Acessou a home");
 });
 
-router.get('/about',(req,res)=>{
+router.get('/about', (req, res) => {
   res.render('pages/about');
+  console.log("Acessou o Sobre");
 });
 
-router.get('/cadastro',(req,res)=>{
-    res.render('pages/cadastro');
+router.get('/cadastro', (req, res) => {
+  res.render('pages/cadastro', { users: users });
+  console.log("Acessou o Cadastro");
 })
-router.get('/curriculo',(req,res)=>{
- res.send('Meu currículo');
+
+router.post('/cadastro/update', (req, res) => {
+  users[req.body.id].name = req.body.name;
+  users[req.body.id].email = req.body.email;
+  users[req.body.id].address = req.body.address;
+  users[req.body.id].age = req.body.age;
+  users[req.body.id].height = req.body.height;
+  users[req.body.id].vote = req.body.vote;
+
+  console.log("Dados recebidos: ", req.body);
+  res.sendStatus(200);
+})
+
+router.get('/cadastro/list', (req, res) => {
+  //lista de usuarios cadastrado
 });
 
-router.get('/cadastro/insert',(req,res)=>{
-    //inserir um usuario
-   });
-   router.get('/cadastro/list',(req,res)=>{
-   //listar de usuarios cadastrado
-   });
 
-
-   //Essa linha permite que este código seja exportado como ummódulo e possa ser usado em outras partes da aplicação.
-   module.exports = router; 
+//Essa linha permite que este código seja exportado como um módulo e possa ser usado em outras partes da aplicação.
+module.exports = router;
