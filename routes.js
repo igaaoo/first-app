c9bf0f0557b7e8b54e9621ab3662ec53389d9d9d
Igor Neves <igorneves0404@gmail.com>
2021-08-26 19:25:16 -0300

    Criação de lista dos usuários

--- routes.js
+++ routes.js
@@ -34,9 +34,15 @@ router.post('/cadastro/update', (req, res) => {
   console.log("Atualizou um cadastro");
 });
 
+global.usersList =[];
 router.get('/list', (req, res) => {
   res.render('pages/list');
-  console.log("Acessou a lista");
+
+  console.log("Acessou a lista / Lista:");
+  for (let cont=0;cont<20;cont++){
+    usersList[cont] = users[cont];
+    console.log(JSON.stringify(usersList[cont]));
+}
 });
 
 router.post('/cadastro/add',(req,res)=>{
